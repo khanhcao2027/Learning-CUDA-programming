35fe1d4aeb9d65778fffdb8488b5567b32ca72b8
Khanh Cao Ngoc Mai <knc006@bucknell.com>
2026-06-07 15:52:20 +0700

    Repo setup + HelloWorld

---
+++ HelloWorld/kernel.cu
@@ -0,0 +1,19 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+__global__ void test01()
+{
+	printf("\n Block ID is %d -- Thread ID is %d", blockIdx.x, threadIdx.x);
+}
+
+int main()
+{
+	test01<<<1, 10>>>();
+	cudaDeviceSynchronize();
+	return 0;
+}
